7c6d8d63774be1bf6163f26e0f6fbaee65cefc37
Alexandre <alex.desvallees@gmail.com>
2019-04-04 13:11:47 +0200

    correction du devoir de ce matin

--- ControleContinu/04-04-2019.py
+++ ControleContinu/04-04-2019.py
@@ -3,23 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from datetime import datetime
 
-x=[["2019-03-16"], ["2019-03-18"], ["2019-03-20"], ["2019-03-24"], ["2019-03-25"], ["2019-03-26"], ["2019-03-27"], ["2019-03-28"], ["2019-03-30"], ["2019-04-03"]]
-# convX = x.astype(np.float64)
-y=[[81682], [81720], [81760], [81826], [81844], [81864], [81881], [81900], [81933], [82003]]
+def date_to_float(year, month, day) :
+    x = datetime(year, month, day)
+    return (datetime.timestamp(x))
+
+x=[[date_to_float(2019, 3, 16)], [date_to_float(2019, 3, 18)], [date_to_float(2019, 3, 20)], [date_to_float(2019, 3, 24)], [date_to_float(2019, 3, 25)], [date_to_float(2019, 3, 26)], [date_to_float(2019, 3, 27)], [date_to_float(2019, 3, 28)], [date_to_float(2019, 3, 30)], [date_to_float(2019, 4, 3)]]
+y=[[81682.0], [81720.0], [81760.0], [81826.0], [81844.0], [81864.0], [81881.0], [81900.0], [81933.0], [82003.0]]
 
 linearRegressor = LinearRegression()
 reg= linearRegressor.fit(x, y)
 
+z = linearRegressor.predict([[date_to_float(2019, 4, 4)]])[0][0]
+print("Prédiction pour le 2019-04-04 : %s" % z)
+
 plt.subplot(211)
 plt.scatter(x, y, color = 'red')
+plt.scatter([date_to_float(2019, 4, 4)], linearRegressor.predict([[date_to_float(2019, 4, 4)]]), color = 'purple')
 plt.plot(x, linearRegressor.predict(x), color = 'blue')
+plt.title("Courbe de regression linéaire")
 
 plt.subplot(212)
 plt.scatter(x, y - linearRegressor.predict(x), color = 'red')
 plt.axhline(0, color='blue')
+plt.title("Courbe des résidus")
 
 plt.show()
-
-# Exemple de regression lineaire repris de ce que l'on avait fait
-# Convertion des dates en valeures flotantes en cours, pas de compteurs
